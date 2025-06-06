8825432c496dc2fd66b3856f01be8f1e8fb3c619
GrootNRoll <mambas32584@gmail.com>
2025-06-06 10:35:31 +0300

    Updates

--- recipe-app/src/utils/constants.ts
+++
@@ -1,6 +0,0 @@
-export const RECIPE_TYPES = [
-  { value: 'breakfast', label: 'Завтрак' },
-  { value: 'lunch', label: 'Обед' },
-  { value: 'dinner', label: 'Ужин' },
-  { value: 'other', label: 'Другое' },
-];
